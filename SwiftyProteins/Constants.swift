ca30030eb424317d0b4979ef6928f2801b4cfa18
Alex Legent <alegent@student.42.fr>
2018-03-16 15:12:49 +0100

    Adding CPK Colors

--- SwiftyProteins/Constants.swift
+++ SwiftyProteins/Constants.swift
@@ -11,4 +11,32 @@ import ToolboxLGNT
 
 struct Colors {
     static let background = UIColor(r: 246, g: 246, b: 246)
+    static let CPK: [AtomType: UIColor] = [
+        .AS: UIColor(r: 189, g: 132, b: 225),
+        .B: UIColor(r: 253, g: 189, b: 184),
+        .BR: UIColor(r: 166, g: 37, b: 40),
+        .C: UIColor(r: 102, g: 102, b: 102),
+        .CA: UIColor(r: 72, g: 253, b: 47),
+        .CL: UIColor(r: 47, g: 238, b: 51),
+        .CO: UIColor(r: 239, g: 145, b: 163),
+        .CR: UIColor(r: 140, g: 155, b: 199),
+        .F: UIColor(r: 131, g: 182, b: 252),
+        .FE: UIColor(r: 224, g: 102, b: 56),
+        .H: UIColor(r: 246, g: 246, b: 246),
+        .I: UIColor(r: 148, g: 19, b: 147),
+        .MG: UIColor(r: 143, g: 253, b: 49),
+        .MN: UIColor(r: 157, g: 125, b: 198),
+        .MO: UIColor(r: 88, g: 182, b: 182),
+        .N: UIColor(r: 14, g: 37, b: 251),
+        .O: UIColor(r: 252, g: 16, b: 29),
+        .P: UIColor(r: 253, g: 129, b: 35),
+        .PT: UIColor(r: 245, g: 238, b: 211),
+        .RH: UIColor(r: 19, g: 126, b: 140),
+        .RU: UIColor(r: 38, g: 144, b: 144),
+        .S: UIColor(r: 180, g: 179, b: 36),
+        .SE: UIColor(r: 253, g: 162, b: 40),
+        .V: UIColor(r: 168, g: 168, b: 172),
+        .W: UIColor(r: 39, g: 151, b: 213),
+        .Other: UIColor(r: 221, g: 119, b: 255)
+    ]
 }
