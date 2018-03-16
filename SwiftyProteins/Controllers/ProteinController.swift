4a335fd7a60c1cfbc709d10724981adfcd553a0c
Alex Legent <alegent@student.42.fr>
2018-03-16 16:38:39 +0100

    Setup scene and printing a box ... it's easy m'kay !

--- SwiftyProteins/Controllers/ProteinController.swift
+++ SwiftyProteins/Controllers/ProteinController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import ToolboxLGNT
+import SceneKit
 
 final class ProteinController: GenericViewController {
     var ligand: Ligand? {
@@ -17,9 +18,50 @@ final class ProteinController: GenericViewController {
         }
     }
     
+    private let scene: SCNScene = {
+        let camera = SCNNode()
+        let scene = SCNScene()
+        camera.camera = SCNCamera()
+        camera.position = SCNVector3(0, 0, 10)
+        scene.rootNode.addChildNode(camera)
+        return scene
+    }()
+    
+    private lazy var sceneView: SCNView = {
+        let view = SCNView()
+        view.allowsCameraControl = true
+        view.scene = scene
+        
+        // TODO: Remove unused parameters
+        view.showsStatistics = true
+        // view.autoenablesDefaultLighting = true
+        
+        return view
+    }()
+    
+    // MARK:- View lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // TODO: Remove debug form
+        // DEBUG
+        let box = SCNBox()
+        let node = SCNNode(geometry: box)
+        node.position = SCNVector3(0, 0, 0)
+        box.firstMaterial?.diffuse.contents = UIColor.green
+        scene.rootNode.addChildNode(node)
+        // END DEBUG
+    }
+    
     // MARK:- Setups
     override func setupViews() {
         super.setupViews()
         view.backgroundColor = Colors.background
+        view.addSubview(sceneView)
+    }
+    
+    override func setupLayouts() {
+        super.setupLayouts()
+        _ = sceneView.fill(view.safeAreaLayoutGuide)
     }
 }
